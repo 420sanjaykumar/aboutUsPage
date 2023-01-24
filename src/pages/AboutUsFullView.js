4fa062d33a4faa1be10789525fe02f1b5afb7a56
420sanjaykumar <64832308+420sanjaykumar@users.noreply.github.com>
2023-01-24 22:20:11 +0530

    commit

---
+++ src/pages/AboutUsFullView.js
@@ -0,0 +1,278 @@
+import styles from "./AboutUsFullView.module.css";
+
+const AboutUsFullView = () => {
+  return (
+    <div className={styles.aboutUsFullView}>
+      <div className={styles.aboutUsFullViewInner}>
+        <div className={styles.groupParent}>
+          <div className={styles.vectorLogoParent}>
+            <img className={styles.vectorLogoIcon} alt="" />
+            <img className={styles.groupChild} alt="" src="../group-2.svg" />
+            <img className={styles.groupItem} alt="" src="../group-603.svg" />
+            <img className={styles.groupInner} alt="" src="../group-587.svg" />
+            <div className={styles.rectangleParent}>
+              <div className={styles.rectangleDiv} />
+              <div className={styles.faqs}>FAQs</div>
+            </div>
+            <img className={styles.groupIcon} alt="" src="../group-602.svg" />
+          </div>
+          <img
+            className={styles.vectorLogoIcon1}
+            alt=""
+            src="../vector-logo.svg"
+          />
+          <div className={styles.groupChild1} />
+        </div>
+      </div>
+      <img
+        className={styles.aboutUsFullViewChild}
+        alt=""
+        src="../group-744.svg"
+      />
+      <div className={styles.header}>
+        <div className={styles.headerChild} />
+        <div className={styles.groupContainer}>
+          <div className={styles.slietLongowalsAnnualTechniParent}>
+            <i className={styles.slietLongowalsAnnual}>
+              SLIET Longowal’s Annual Technical Fest
+            </i>
+            <i className={styles.techfest22}>
+              <span>techFEST’</span>
+              <span className={styles.span}>23</span>
+              <span>{` `}</span>
+            </i>
+          </div>
+          <div className={styles.caProgram}>CA Program</div>
+          <div className={styles.domains}>
+            <img
+              className={styles.keyboardArrowDownIcon}
+              alt=""
+              src="../keyboard-arrow-down.svg"
+            />
+            <div className={styles.domains1}>Domains</div>
+          </div>
+          <div className={styles.workshops}>Workshops</div>
+          <div className={styles.aboutUs}>About Us</div>
+          <div className={styles.signIn}>
+            <div className={styles.signIn1}>Sign in</div>
+            <button className={styles.signInChild} />
+          </div>
+          <div className={styles.lineDiv} />
+        </div>
+      </div>
+      <img className={styles.icon} alt="" src="../6417740-1@2x.png" />
+      <div className={styles.frameDiv}>
+        <div className={styles.heroParent}>
+          <div className={styles.hero}>
+            <div className={styles.lineParent}>
+              <div className={styles.groupChild2} />
+              <img
+                className={styles.whiteSliet1}
+                alt=""
+                src="../white-sliet-1@2x.png"
+              />
+            </div>
+            <div
+              className={styles.subtitle}
+            >{`techFEST is the platform where engineers, innovators, changemakers, entrepreneurs and tech enthusiasts collaborate to create solutions to some of the world's most challenging problems. `}</div>
+          </div>
+          <img
+            className={styles.whatsappImage20230111At1}
+            alt=""
+            src="../whatsapp-image-20230111-at-1526-1@2x.png"
+          />
+          <div className={styles.aboutSliet}>
+            <div className={styles.aboutSlietChild} />
+            <div className={styles.subtitleParent}>
+              <div className={styles.subtitle1}>
+                <p className={styles.santLongowalInstitute}>
+                  Sant Longowal Institute of Engineering and Technology was
+                  established as a tribute to the revered saint Sant Harchand
+                  Singh Longowal. The institute strives
+                </p>
+                <p className={styles.santLongowalInstitute}>
+                  to impart quality knowledge and technical skills by creating a
+                  healthy competitive environment that encourages innovation and
+                  critical thinking among students. At the academic level the
+                  institute offers various academic courses of Undergraduate and
+                  PostGraduate programmes. Apart from academics, SLIET is home
+                  to various co-curricular activities and communities such as
+                  National Cadet Corps - SLIET, Endeavour, Mavericks, SSDC,
+                  Juggernauts, Green rangers, Institute Innovation cell, SRAS,
+                  National Service Scheme, etc.
+                </p>
+              </div>
+              <div className={styles.aboutSliet1}>
+                <span>{`About `}</span>
+                <span className={styles.sliet}>SLIET</span>
+              </div>
+              <div className={styles.groupChild3} />
+            </div>
+          </div>
+          <div className={styles.timeline}>
+            <div className={styles.techfest22Parent}>
+              <i className={styles.techfest221}>techFEST’22</i>
+              <div className={styles.subtitleGroup}>
+                <div className={styles.subtitle2}>
+                  With 40+ Events and an aim to take up the Ultimate Leap in
+                  Technology for Agriculture, this year.
+                </div>
+                <b className={styles.theUltimateLeap}>
+                  The Ultimate Leap to Agri-Tech
+                </b>
+              </div>
+              <img
+                className={styles.groupChild4}
+                alt=""
+                src="../group-767.svg"
+              />
+            </div>
+            <div className={styles.techfest23Parent}>
+              <i className={styles.techfest23}>techFEST’23</i>
+              <div className={styles.subtitleContainer}>
+                <div className={styles.subtitle3}>
+                  With 40+ Events and an aim to take up the Ultimate Leap in
+                  Technology for Agriculture, this year.
+                </div>
+                <b className={styles.innovationInAgriculture}>
+                  Innovation in Agriculture
+                </b>
+              </div>
+              <img
+                className={styles.groupChild5}
+                alt=""
+                src="../group-7671.svg"
+              />
+            </div>
+            <div className={styles.techfest21Parent}>
+              <i className={styles.techfest21}>techFEST’21</i>
+              <div className={styles.groupDiv}>
+                <div className={styles.subtitle4}>
+                  <p className={styles.santLongowalInstitute}>
+                    With 30+ Universities Accross the Nation
+                  </p>
+                  <p className={styles.weEnsuredThat}>
+                    we ensured that together we come up with technologies
+                    Revitalising India
+                  </p>
+                </div>
+                <b className={styles.theUltimateLeap}>Revitalizing India</b>
+              </div>
+              <img
+                className={styles.groupChild6}
+                alt=""
+                src="../group-768@2x.png"
+              />
+            </div>
+            <div className={styles.techfest20Parent}>
+              <i className={styles.techfest20}>techFEST’20</i>
+              <div className={styles.subtitleParent1}>
+                <div className={styles.subtitle5}>
+                  <p className={styles.santLongowalInstitute}>
+                    Instigating the blah abladeifhw eof q qe e
+                  </p>
+                  <p className={styles.weEnsuredThat}>lorem Ipsum</p>
+                </div>
+                <b className={styles.theUltimateLeap}>
+                  Pedalling towards Cosmic Yantra
+                </b>
+              </div>
+              <img
+                className={styles.groupChild7}
+                alt=""
+                src="../group-769@2x.png"
+              />
+            </div>
+            <div className={styles.groupParent1}>
+              <div className={styles.subtitleParent2}>
+                <div className={styles.subtitle5}>
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry.
+                </div>
+                <b className={styles.theUltimateLeap}>
+                  Innovation in Cycle Design
+                </b>
+              </div>
+              <img
+                className={styles.groupChild8}
+                alt=""
+                src="../group-770.svg"
+              />
+              <i className={styles.techfest19}>techFEST’19</i>
+            </div>
+          </div>
+          <div className={styles.gallery}>
+            <div className={styles.gallery1}>GALLERY</div>
+            <img
+              className={styles.galleryChild}
+              alt=""
+              src="../group-568.svg"
+            />
+          </div>
+          <div className={styles.sponsors}>
+            <div className={styles.ourPreviousSponsors}>
+              OUR PREVIOUS SPONSORS
+            </div>
+            <img
+              className={styles.sponsorsChild}
+              alt=""
+              src="../group-5681.svg"
+            />
+          </div>
+          <div className={styles.start}>
+            <textarea className={styles.startChild} />
+          </div>
+        </div>
+        <b
+          className={styles.ourRootsGo}
+        >{`Our Roots go back to 1998 , When We first set out our techFEST `}</b>
+        <img className={styles.asset83x1} alt="" src="../asset-83x-1@2x.png" />
+      </div>
+      <img className={styles.asset24x2} alt="" src="../asset-24x-2@2x.png" />
+      <img className={styles.vectorIcon} alt="" src="../vector.svg" />
+      <img className={styles.vectorIcon1} alt="" src="../vector1.svg" />
+      <img className={styles.vectorIcon2} alt="" src="../vector2.svg" />
+      <img className={styles.vectorIcon3} alt="" src="../vector3.svg" />
+      <img className={styles.icon1} alt="" src="../icon.svg" />
+      <img className={styles.statsSecIcon} alt="" src="../stats-sec.svg" />
+      <div className={styles.rectangleGroup}>
+        <div className={styles.groupChild9} />
+        <div className={styles.image16Parent}>
+          <img className={styles.image16Icon} alt="" src="../image-16@2x.png" />
+          <div className={styles.div}>30+</div>
+          <div className={styles.institutesParticipateEvery}>
+            Institutes Participate every year
+          </div>
+        </div>
+        <div className={styles.image17Parent}>
+          <img className={styles.image17Icon} alt="" src="../image-17@2x.png" />
+          <div className={styles.k}>10k +</div>
+          <div className={styles.footfallDuringTechfestContainer}>
+            <p className={styles.santLongowalInstitute}>{`Footfall during `}</p>
+            <p className={styles.weEnsuredThat}>techFEST 21</p>
+          </div>
+        </div>
+        <div className={styles.image18Parent}>
+          <img className={styles.image18Icon} alt="" src="../image-18@2x.png" />
+          <div className={styles.l}>5L +</div>
+          <div className={styles.worthOfGoodiesContainer}>
+            <p className={styles.santLongowalInstitute}>Worth of goodies</p>
+            <p className={styles.weEnsuredThat}>and prize money</p>
+          </div>
+        </div>
+        <img className={styles.statsSecIcon1} alt="" src="../stats-sec1.svg" />
+        <img className={styles.statsSecIcon2} alt="" src="../stats-sec2.svg" />
+      </div>
+      <img className={styles.vectorIcon4} alt="" src="../vector4.svg" />
+      <div className={styles.footer}>
+        <div className={styles.footerChild} />
+        <div className={styles.techfestslietParent}>
+          <b className={styles.techfestsliet}>/techfestsliet</b>
+          <img className={styles.groupChild10} alt="" src="../group-457.svg" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutUsFullView;
